2580405b1915f92d57783d9d78e44d66a2b363da
legionpunisher <mbuguamoses656@gmail.com>
2020-07-16 13:35:47 +0300

    modified js script

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,6 @@ var add = function(number1, number2) {
     return number1 + number2;
 };
 var number1 = parseInt(prompt("enter a number"));
-var number2 = parseInt(prompt("enter another number");
-        var result = add(number1, number2); alert(result);
+var number2 = parseInt(prompt("enter another number"));
+var result = add(number1, number2);
+alert(result);
